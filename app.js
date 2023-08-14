8237609babfef2c88a135f25408a9fa58769a36f
hakim112211 <maulanahakimrizki@gmail.com>
2023-08-14 16:05:21 +0700

    membuat proposal

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const pug = require('pug')
+const port = 3000;
+
+app.set('view engine', 'pug');
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.render('index', { title: 'Portfolio' });
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
